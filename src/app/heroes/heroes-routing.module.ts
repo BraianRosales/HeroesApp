98f8baee4da1672889384e79e05a27052c2162c0
BraianRosales <braianezequielrosales@gmail.com>
2022-05-22 23:06:54 -0300

    Mi primer commit, creando rutas principales y rutas hijas, aprendiendo lazyLoading.

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+import { AddComponent } from './pages/add/add.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'listado',
+        component: ListComponent,
+      },
+      {
+        path: 'agregar',
+        component: AddComponent,
+      },
+      {
+        path: 'editar/id',
+        component: AddComponent,
+      },
+      {
+        path: 'buscar',
+        component: SearchComponent,
+      },
+      {
+        path: 'id',
+        component: HeroeComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'listado',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HeroesRoutingModule {}
